04b474141829c479290ff3bb2adb7aee7889cc0f
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-02-12 16:34:30 -0500

    Updated server.js file

--- server/server.js
+++ server/server.js
@@ -4,31 +4,36 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const fs = require('fs'); // File system
+
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+app.use("/", express.static('public'));
 
+const budgetData = JSON.parse(fs.readFileSync('budget-data.json')); // Read and parse the budget data
+
+// const budget = {
+//     myBudget: [
+//         {
+//             title: 'Eat out',
+//             budget: 25
+//         },
+//         {
+//             title: 'Rent',
+//             budget: 275
+//         },
+//         {
+//             title: 'Grocery',
+//             budget: 110
+//         },
+//     ]
+// };
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgetData);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
